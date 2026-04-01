4cc8e2f6bccd51d326c93e1fb828bfafa76156f2
StyleSync Developer <dev@stylesync.local>
2026-04-01 15:24:39 +0530

    Add backend/src/auth-engine/core/events/index.js

---
+++ backend/src/auth-engine/core/events/index.js
@@ -0,0 +1,23 @@
+const EventEmitter = require('events');
+
+class AuthEventEmitter extends EventEmitter { }
+
+const eventEmitter = new AuthEventEmitter();
+
+module.exports = {
+    emit: (eventName, data) => eventEmitter.emit(eventName, data),
+    on: (eventName, listener) => eventEmitter.on(eventName, listener),
+    // Common event names
+    EVENTS: {
+        LOGIN_SUCCESS: 'loginSuccess',
+        LOGIN_FAILURE: 'loginFailure',
+        SESSION_CREATED: 'sessionCreated',
+        SESSION_REVOKED: 'sessionRevoked',
+        TOKEN_REFRESHED: 'tokenRefreshed',
+        POLICY_DENIED: 'policyDenied',
+        PASSWORD_RESET_REQUESTED: 'passwordResetRequested',
+        PASSWORD_RESET_COMPLETED: 'passwordResetCompleted',
+        EMAIL_VERIFICATION_REQUESTED: 'emailVerificationRequested',
+        EMAIL_VERIFICATION_COMPLETED: 'emailVerificationCompleted'
+    }
+};
